83b8c111b0fabb6943d7fe041fc12928d2259a7f
Esther-Oteyo <oteyoesther38@gmail.com>
2024-08-12 13:12:01 +0300

    first commit

---
+++ quiz3.js
@@ -0,0 +1,5 @@
+let x=5;
+let y=10;
+
+let sum = x+y;
+console.log("The results of adding x and y:"+sum);
---
+++ quiz5.js
@@ -0,0 +1,6 @@
+let name="john Doe";
+let age=35;
+let isStudent = false;
+console.log(name);
+console.log(age);
+console.log(isStudent);
